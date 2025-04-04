c1ec81c72903b13621e4299d4e205016c34456bf
Miguel Vencato <miguelvencato.cupim@gmail.com>
2025-04-04 20:54:34 -0300

    Agora o carro tem mais marchas

--- Veiculo.ts
+++ Veiculo.ts
@@ -2,7 +2,7 @@ export class Veiculo{
     marca: string = 'Padrão';
     modelo: string = 'Padrão';
     potencia: number = 0;
-    numeroMarchas: number = 5;
+    numeroMarchas: number = 6;
     marchaAtual: number = 0;
     velocidade: number = 0;
 }
